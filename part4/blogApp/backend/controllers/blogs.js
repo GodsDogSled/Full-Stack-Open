a54782fed9609b1fe929cd6a3e1e1cdc4694599c
GodsDogSled <gabekelly@hotmail.ca>
2024-03-24 15:02:37 -0700

    token authentification for blog posts added

--- part4/blogApp/backend/controllers/blogs.js
+++ part4/blogApp/backend/controllers/blogs.js
@@ -1,39 +1,55 @@
 const blogRouter = require('express').Router()
 const { request, response } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
 
-// blogRouter.get('/', (request, response) => {
-//   console.log(response)
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
+  if (authorization && authorization.startsWith('Bearer ')) {
+    console.log(authorization.replace('Bearer ', ''))
+    return authorization.replace('Bearer ', '')
+  }
+  console.log(authorization)
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
-  const blog = await Blog.find({})
+  const blog = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blog)
 })
 
 blogRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-
-  if (!blog.likes) {
-    blog.likes = 0;
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log(decodedToken)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
-  if (!blog.url || !blog.author) {
-    console.log(blog.title)
+  const user = await User.findById(decodedToken.id)
+  const newBlog = new Blog({
+    ...request.body,
+    user: user.id
+  })
+
+  if (!newBlog.likes) {
+    newBlog.likes = 0;
+  }
+
+  if (!newBlog.url || !newBlog.author) {
+    console.log(newBlog.title)
     response.status(400).end()
   } else {
-    const savedBlog = await blog.save()
+
+    const savedBlog = await newBlog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   }
-
-
 })
 
 blogRouter.delete('/:id', async (request, response) => {
@@ -48,7 +64,7 @@ blogRouter.put('/:id', async (request, response) => {
     title: body.title,
     author: body.title,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
   }
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.json(updatedBlog)
--- part4/blogApp/backend/models/user.js
+++ part4/blogApp/backend/models/user.js
@@ -9,10 +9,10 @@ const userSchema = new mongoose.Schema({
   },
   name: String,
   passwordHash: String,
-  blogPosts: [
+  blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Note'
+      ref: 'Blog'
     }
   ],
 })
